0b1219ff4196c7c3ab1540678a38d983942224ab
ADSZ42 <nnancrs1@gmail.com>
2024-12-04 15:00:43 -0800

    day 3

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+document.getElementById('dayBtn').addEventListener('click', dayBtn);
+document.getElementById('weekBtn').addEventListener('click', weekBtn);
+document.getElementById('monthBtn').addEventListener('click', monthBtn);
+
+let WorkC = document.getElementById("WorkC");
+let WorkP = document.getElementById("WorkP");
+
+let PlayC = document.getElementById("PlayC");
+let PlayP = document.getElementById("PlayP");
+
+let StudyC = document.getElementById("StudyC");
+let StudyP = document.getElementById("StudyP");
+
+let ExerC = document.getElementById("ExerC");
+let ExerP = document.getElementById("ExerP");
+
+let SociC = document.getElementById("SociC");
+let SociP = document.getElementById("SociP");
+
+let SelfC = document.getElementById("SelfC");
+let SelfP = document.getElementById("SelfP");
