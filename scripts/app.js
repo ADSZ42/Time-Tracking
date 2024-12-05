b360e3c088cc8bb7f8d9ccf82c7915a08704a6d1
ADSZ42 <nnancrs1@gmail.com>
2024-12-05 15:48:37 -0800

    THR Dy AB

--- scripts/app.js
+++ scripts/app.js
@@ -19,3 +19,59 @@ let SociP = document.getElementById("SociP");
 
 let SelfC = document.getElementById("SelfC");
 let SelfP = document.getElementById("SelfP");
+
+let data;
+
+function apiCall() {
+    fetch('../data/data.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            data = jsonData;
+        })
+}
+
+function updateUI(timeFrame) {
+    data.forEach(item => {
+        let currentData = item.timeframes[timeFrame].current;
+        let previousData = item.timeframes[timeFrame].previous;
+
+        if (item.title === "Work") {
+            WorkC.innerText = currentData + " hrs";
+            WorkP.innerText = "Last Week - " + previousData + " hrs";
+        }
+        if (item.title === "Play") {
+            PlayC.innerText = currentData + " hrs";
+            PlayP.innerText = "Last Week - " + previousData + " hrs";
+        }
+        if (item.title === "Study") {
+            StudyC.innerText = currentData + " hrs";
+            StudyP.innerText = "Last Week - " + previousData + " hrs";
+        }
+        if (item.title === "Exercise") {
+            ExerC.innerText = currentData + " hrs";
+            ExerP.innerText = "Last Week - " + previousData + " hrs";
+        }
+        if (item.title === "Social") {
+            SociC.innerText = currentData + " hrs";
+            SociP.innerText = "Last Week - " + previousData + " hrs";
+        }
+        if (item.title === "Self Care") {
+            SelfC.innerText = currentData + " hrs";
+            SelfP.innerText = "Last Week - " + previousData + " hrs";
+        }
+    });
+}
+
+function dayBtn() {
+    updateUI('daily');
+}
+
+function weekBtn() {
+    updateUI('weekly');
+}
+
+function monthBtn() {
+    updateUI('monthly');
+}
+
+apiCall();
